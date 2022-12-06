2707cdb05eb911a39318a66059a2ea3ecd045bc4
Антон <114058810+PeE2016@users.noreply.github.com>
2022-12-07 01:02:48 +0300

    Update transition.py

--- Проект/Sprint 4/Code/transition.py
+++ Проект/Sprint 4/Code/transition.py
@@ -1,8 +1,10 @@
 import pygame
 from settings import *
 
+
 class Transition:
     def __init__(self, reset, player):
+
         # setup
         self.display_surface = pygame.display.get_surface()
         self.reset = reset
@@ -14,7 +16,6 @@ class Transition:
         self.speed = -2
 
     def play(self):
-
         self.color += self.speed
         if self.color <= 0:
             self.speed *= -1
@@ -24,6 +25,7 @@ class Transition:
             self.color = 255
             self.player.sleep = False
             self.speed = -2
+
         self.image.fill((self.color, self.color, self.color))
         self.display_surface.blit(self.image, (0, 0), special_flags=pygame.BLEND_RGBA_MULT)
 
