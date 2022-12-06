66bf80263726bd70f312243d38ea32170e96baf3
Антон <114058810+PeE2016@users.noreply.github.com>
2022-12-07 01:01:19 +0300

    Update support.py

--- Проект/Sprint 4/Code/support.py
+++ Проект/Sprint 4/Code/support.py
@@ -3,23 +3,25 @@ import pygame
 
 
 def import_folder(path):
-	surface_list = []
+    surface_list = []
 
-	for _, __, img_files in walk(path):
-		for image in img_files:
-			full_path = path + '/' + image
-			image_surf = pygame.image.load(full_path).convert_alpha()
-			surface_list.append(image_surf)
+    for _, __, img_files in walk(path):
+        for image in img_files:
+            full_path = path + '/' + image
+            image_surf = pygame.image.load(full_path).convert_alpha()
+            surface_list.append(image_surf)
 
-	return surface_list
+    return surface_list
 
 
 def import_folder_dict(path):
-	surface_dict = {}
-	for _, __, img_files in walk(path):
-		for image in img_files:
-			full_path = path + '/' + image
-			image_surf = pygame.image.load(full_path).convert_alpha()
-			surface_dict[image.split('.')[0]] = image_surf
-	return surface_dict
+    surface_dict = {}
+
+    for _, __, img_files in walk(path):
+        for image in img_files:
+            full_path = path + '/' + image
+            image_surf = pygame.image.load(full_path).convert_alpha()
+            surface_dict[image.split('.')[0]] = image_surf
+
+    return surface_dict
 
