bc2699ed7af79ddcd773b0b3a23a2ddf5a814d39
Антон <114058810+PeE2016@users.noreply.github.com>
2022-12-07 01:00:09 +0300

    Update sprites.py

--- Проект/Sprint 4/Code/sprites.py
+++ Проект/Sprint 4/Code/sprites.py
@@ -1,5 +1,5 @@
 import pygame
-from settings import*
+from settings import *
 from random import randint, choice
 from timer import Timer
 
@@ -14,7 +14,7 @@ class Generic(pygame.sprite.Sprite):
 
 
 class Interaction(Generic):
-    def __init__(self, pos ,size, groups, name):
+    def __init__(self, pos, size, groups, name):
         surf = pygame.Surface(size)
         super().__init__(pos, surf, groups)
         self.name = name
@@ -22,43 +22,43 @@ class Interaction(Generic):
 
 class Water(Generic):
     def __init__(self, pos, frames, groups):
-
-        # animation
+        # animation setup
         self.frames = frames
-        self.frames_index = 0
+        self.frame_index = 0
 
-        # sprite
-        super().__init__(pos = pos,
-                         surf = self.frames[self.frames_index],
-                         groups = groups,
-                         z = LAYERS['water'])
+        # sprite setup
+        super().__init__(
+            pos=pos,
+            surf=self.frames[self.frame_index],
+            groups=groups,
+            z=LAYERS['water'])
 
-    def animate(self,dt):
-        self.frames_index += 5*dt
-        if self.frames_index >= len(self.frames):
-            self.frames_index = 0
-        self.image = self.frames[int(self.frames_index)]
+    def animate(self, dt):
+        self.frame_index += 5 * dt
+        if self.frame_index >= len(self.frames):
+            self.frame_index = 0
+        self.image = self.frames[int(self.frame_index)]
 
     def update(self, dt):
-            self.animate(dt)
+        self.animate(dt)
 
 
 class WildFlower(Generic):
     def __init__(self, pos, surf, groups):
         super().__init__(pos, surf, groups)
-        self.hitbox = self.rect.copy().inflate(-20, -self.rect.height * 0.1)
+        self.hitbox = self.rect.copy().inflate(-20, -self.rect.height * 0.9)
 
 
 class Particle(Generic):
-    def __init__(self, pos, surf, groups, z, duration = 200):
+    def __init__(self, pos, surf, groups, z, duration=200):
         super().__init__(pos, surf, groups, z)
         self.start_time = pygame.time.get_ticks()
         self.duration = duration
 
-        # White
+        # white surface 
         mask_surf = pygame.mask.from_surface(self.image)
         new_surf = mask_surf.to_surface()
-        new_surf.set_colorkey((0,0,0))
+        new_surf.set_colorkey((0, 0, 0))
         self.image = new_surf
 
     def update(self, dt):
@@ -72,58 +72,61 @@ class Tree(Generic):
         super().__init__(pos, surf, groups)
 
         # tree attributes
-        self.health = 4
+        self.health = 5
         self.alive = True
-
-        self.stump_surf = pygame.image.load(f'graphics/stumps/{"small" if name == "Small" else "large"}.png').convert_alpha()
-        self.invul_timer = Timer(200)
+        stump_path = f'graphics/stumps/{"small" if name == "Small" else "large"}.png'
+        self.stump_surf = pygame.image.load(stump_path).convert_alpha()
 
         # apples
-        self.apples_surf = pygame.image.load('graphics/fruit/apple.png')
-        self.apples_pos = APPLE_POS[name]
+        self.apple_surf = pygame.image.load('graphics/fruit/apple.png')
+        self.apple_pos = APPLE_POS[name]
         self.apple_sprites = pygame.sprite.Group()
         self.create_fruit()
 
         self.player_add = player_add
 
-    def damage(self):
-        self.health-=1
+        # sounds
+        self.axe_sound = pygame.mixer.Sound('audio/axe.mp3')
 
-        if len(self.apple_sprites.sprites())>0:
+    def damage(self):
+
+        # damaging the tree
+        self.health -= 1
+
+        # play sound
+        self.axe_sound.play()
+
+        # remove an apple
+        if len(self.apple_sprites.sprites()) > 0:
             random_apple = choice(self.apple_sprites.sprites())
-            Particle(pos = random_apple.rect.topleft,
-                     surf= random_apple.image,
-                     groups= self.groups()[0],
-                     z = LAYERS['fruit'],
-                     duration = 200)
+            Particle(
+                pos=random_apple.rect.topleft,
+                surf=random_apple.image,
+                groups=self.groups()[0],
+                z=LAYERS['fruit'])
             self.player_add('apple')
             random_apple.kill()
 
-    # Рандомная генерация яблок !!! может пригодится
-    def create_fruit(self):
-        for pos in self.apples_pos:
-            if randint(0,10)<2:
-                x = pos[0] + self.rect.left
-                y = pos[1] + self.rect.top
-                Generic(
-                    pos=(x, y),
-                    surf=self.apples_surf,
-                    groups=[self.apple_sprites, self.groups()[0]],
-                    z=LAYERS['fruit'])
-
     def check_death(self):
         if self.health <= 0:
-            Particle(pos=self.rect.topleft,
-                     surf=self.image,
-                     groups=self.groups()[0],
-                     z=LAYERS['fruit'],
-                     duration=500)
+            Particle(self.rect.topleft, self.image, self.groups()[0], LAYERS['fruit'], 300)
             self.image = self.stump_surf
-            self.rect = self.image.get_rect(midbottom = self.rect.midbottom)
-            self.hitbox = self.rect.copy().inflate(-10,-self.rect.height*0.6)
+            self.rect = self.image.get_rect(midbottom=self.rect.midbottom)
+            self.hitbox = self.rect.copy().inflate(-10, -self.rect.height * 0.6)
             self.alive = False
             self.player_add('wood')
 
     def update(self, dt):
         if self.alive:
             self.check_death()
+
+    def create_fruit(self):
+        for pos in self.apple_pos:
+            if randint(0, 10) < 2:
+                x = pos[0] + self.rect.left
+                y = pos[1] + self.rect.top
+                Generic(
+                    pos=(x, y),
+                    surf=self.apple_surf,
+                    groups=[self.apple_sprites, self.groups()[0]],
+                    z=LAYERS['fruit'])
