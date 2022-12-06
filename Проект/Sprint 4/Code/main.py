1fe5ca87ac3393db5415b2dc31ec012c61feda57
Антон <114058810+PeE2016@users.noreply.github.com>
2022-12-07 00:52:19 +0300

    Update main.py

--- Проект/Sprint 4/Code/main.py
+++ Проект/Sprint 4/Code/main.py
@@ -2,20 +2,21 @@ import pygame, sys
 import button
 from settings import *
 from level import Level
+from player import *
 
 pygame.mixer.pre_init(44100, -16, 2, 512)
 pygame.init()
 
-# загружаем фоновую музыку
+# music
 menu_music = pygame.mixer.Sound('music/menumusic.wav')
 gameplay_music = pygame.mixer.Sound('music/gameplaymusic.wav')
 
 
-# класс игры
+# game
 class Game:
     def __init__(self):
 
-        # инициализации библиотеки и игрового окна
+        # initialization
         self.menu_runs = True
         self.gameplay_runs = False
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -25,12 +26,14 @@ class Game:
 
     def run(self):
         while True:
+            clock = pygame.time.Clock()
+            clock.tick(FPS)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
 
-                # выход из геймплея обратно в меню
+                # exit
                 if self.gameplay_runs == True:
                     if event.type == pygame.KEYDOWN:
                         if event.key == pygame.K_BACKSPACE:
@@ -38,31 +41,31 @@ class Game:
                             self.gameplay_runs = False
                             self.menu_runs = True
 
-            # состояние геймплея
+            # gameplay status
             if self.gameplay_runs == True:
-                gameplay_music.set_volume(0.5)
+                gameplay_music.set_volume(0.1)
                 gameplay_music.play(-1)
                 dt = self.clock.tick() / 1000
                 self.level.run(dt)
                 pygame.display.update()
 
-            # состояние меню
+            # menu status
             elif self.menu_runs == True:
 
-                # загружаем фоновое изображение для меню
+                # menu
                 menu_background = pygame.image.load('backgrounds/mainmenu_background.jpg')
 
-                # загружаем png-изображения кнопок в переменные
+                # buttons
                 start_image = pygame.image.load('buttons/button_start.png').convert_alpha()
                 load_image = pygame.image.load('buttons/button_load.png').convert_alpha()
                 exit_image = pygame.image.load('buttons/button_exit.png').convert_alpha()
 
-                # создание объектов-кнопок
+                # buttons-object
                 start_button = button.Button(451, 217, start_image)
                 load_button = button.Button(451, 302, load_image)
                 exit_button = button.Button(451, 387, exit_image)
 
-                # создание меню и возможных вариантов сценариев нажатия на его кнопки
+                # menu click
                 self.screen.blit(menu_background, (0, 0))
                 menu_music.set_volume(0.5)
                 menu_music.play(-1)
